98b5fe46b45bb894df449d343d76a846c1df71fb
Bolimzincbd <seakwiqww@gmail.com>
2026-04-20 00:03:42 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () { return view('home'); }); // Create a home view using the theme's index body
+Route::get('/', function () { 
+    return view('home'); 
+});
+
 Route::resource('products', ProductController::class);
